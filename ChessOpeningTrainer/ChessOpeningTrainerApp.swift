48d0f8b456d8d4222fb920c4ae70380b9bc3b3d5
Christian Gleissner <ch.gleissner@tum.de>
2023-04-19 15:25:44 +0200

    Initial Commit

---
+++ ChessOpeningTrainer/ChessOpeningTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessOpeningTrainerApp.swift
+//  ChessOpeningTrainer
+//
+//  Created by Christian Gleißner on 19.04.23.
+//
+
+import SwiftUI
+
+@main
+struct ChessOpeningTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChessOpeningTrainer/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChessOpeningTrainer
+//
+//  Created by Christian Gleißner on 19.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
